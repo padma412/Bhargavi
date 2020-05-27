76551e3d9d8013da7c61e65e25f0de3a96935353
Bhargavi <182g1a0412.ece@gmail.com>
2020-05-27 11:43:34 +0530

    google layout

--- app/src/main/java/com/example/assignment_2/MainActivity.kt
+++ app/src/main/java/com/example/assignment_2/MainActivity.kt
@@ -5,8 +5,10 @@ import android.os.Parcel
 import android.os.Parcelable
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -15,14 +17,28 @@ import kotlinx.android.synthetic.main.activity_main.*
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
-        setSupportActionBar(toolbar)
+         setSupportActionBar(toolbar)
+         val edit1 = findViewById<EditText>(R.id.Edittext1)
+         val edit2 = findViewById<EditText>(R.id.Edittext2)
          val Button =findViewById(R.id.button) as Button
-         val View = findViewById(android.R.id.content) as View
-         Button.setOnClickListener(new View.OnClickListner) {
-            @Override
-            void onClick(View v)
+        Button.setOnClickListener({
+            val uname = edit1.text.toString()
+            val pwd = edit2.text.toString()
+            if (uname == "testusername" && pwd == "testpassword") {
+                val snack = Snackbar.make(it, "Login Success", Snackbar.LENGTH_LONG)
+                snack.setAction("OK") {
+                    println("Login ")
+                }
+                snack.show()
+            }
+            else {
+                val snackbar = Snackbar.make(it, "Username/Password is incorrect",Snackbar.LENGTH_LONG)
+                snackbar.show()
+
+            }
+
+        })
          }
  }
 
 
-}
